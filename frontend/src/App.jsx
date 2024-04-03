f31f763f9b734bbaf3b3511723e6e47ab9706a1a
yernero <jacobmajor@aol.com>
2024-04-02 21:34:03 -0600

    1:12 navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import react from "react"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoutes"
 
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login"/>
+}
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
